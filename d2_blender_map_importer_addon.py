4094f25fb179d218bffab69a97d56a4001039b3e
DeltaDesigns <50308149+DeltaDesigns@users.noreply.github.com>
2023-10-10 11:26:13 -0400

    Update d2_blender_map_importer_addon.py

--- d2_blender_map_importer_addon.py
+++ d2_blender_map_importer_addon.py
@@ -1,7 +1,7 @@
 bl_info = {
     "name": "Destiny 2 Map Importer",
     "author": "DeltaDesigns, Montague/Monteven",
-    "version": (0, 4, 1),
+    "version": (0, 4, 2),
     "blender": (3, 0, 0),
     "location": "File > Import",
     "description": "Import Destiny 2 Maps exported from Charm",
